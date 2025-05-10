523e1a7166b83148f30e4700235e3e97f271bb64
Arthur Herry <arthur.herry@ynov.com>
2025-05-10 17:19:18 +0200

    ajout systeme du morpion

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
 
+let data = ["","","","","","","","",""];
 const TicTacToe = () => {
-    return (
-        <div>
 
+    let [count,setCount] = useState(0);
+    let [lock,setLock] = useState(false);
+    
+    const toggle = (w,num) => {
+        if (lock) {
+            return 0 
+        }
+        if(count%2===0)
+        {
+            e.target.innerHTML = `<img src='${cross_icon}'>`;
+            data[num]="x";
+            setCount(++count);
+        }    
+        else{
+            e.target.innerHTML = `<img src='${circle_icon}'>`;
+            data[num]="o";
+            setCount(++count);
+        }    
+    }
+
+    const checkWin = () => {
+        if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+        {
+            won(data);
+        }
+    } 
+
+    const won = (winner) => {
+        setLock(true);
+    }
+
+    return (
+        <div classname='container'>
+            <h1 className="title">Le jeu du <span>Morpion </span></h1>
+            <div className="board">
+                <div className="row1">
+                    <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+                <div className="row2">
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+                <div className="row3">
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+            </div>
+            <button className="reset">Recommencer</button>
         </div>
     )
 }
